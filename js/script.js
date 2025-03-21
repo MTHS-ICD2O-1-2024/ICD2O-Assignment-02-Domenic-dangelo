b0cb98fd564ac10a2d7e830afa221c897c28edc2
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-03-21 17:03:34 +0000

    volume of right triangular prism

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: feb 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates volume of a right triangular prism.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function calculateVolumeOfRightTriangularPrism() {
+  // input
+  const baseOfRigthTriangularPrism = parseFloat(document.getElementById("base-of-right-triangular-prism").value)
+  const heightOfRigthTriangularPrism = parseFloat(document.getElementById("height-of-right-triangular-prism").value)
+  const lengthOfRigthTriangularPrism = parseFloat(document.getElementById("length-of-right-triangular-prism").value)
+
+  // process
+  const volumeOfRigthTriangularPrism = baseOfRigthTriangularPrism * heightOfRigthTriangularPrism * lengthOfRigthTriangularPrism / 2
+
+  // output
+  document.getElementById("answer").innerHTML = "Volume is: " + volumeOfRigthTriangularPrism + " cm³"
 }
